198901ef140baebd1dac9f9a411f9c73be182103
Vahid bastani <vahid.bastani.ginevra.dibe.unige.it>
2015-10-16 10:37:10 +0200

    first skeleton of DPGMM class

--- DP-GMM/DPGMM.m
+++ DP-GMM/DPGMM.m
@@ -1,7 +1,35 @@
-classdef DPGMM
+classdef DPGMM < handel
     properties (Access = private)
-        hyper_parameters;
-        conc_parameters; %DP concenteration parameter
+        hyper_params
+        conc_param       % DP concenteration parameter
+        cluster_params   % cluster parameters
+        indicator        % cluster indicator variable
+        cluster_prob     % cluster probabilities
+        data_likelihood
+        data
+    end
+    
+    methods (Access = private)
+        % -------------------- Constructor ---------------------------
+        function obj = DPGMM(varargin)
+        end
+        % ------------------------------------------------------------
+        % ----- Sample indicator variables for Gibbs ----------------- 
+        function sampleIndicatorsGibbs(obj)
+        end
+        % ------------------------------------------------------------
+        % ----- Sample indicator variables for Collapse Gibbs -------- 
+        function sampleIndicatorsCollapseGibbs(obj)
+        end
+        
+        function sampleClusterParameters(obj)
+        end
+        
+        function sampleClusterProbabilities(obj)
+        end
+        
+        function isConverged()
+        end
     end
     
 end
