c790a0d272c3732d017a961ff52c6550ad23063a
Vahid Bastani <bastani.vahid@gmail.com>
2015-11-15 20:35:47 +0100

    constractorTest added

---
+++ test/ConstractorTest.m
@@ -0,0 +1,9 @@
+classdef ConstractorTest < matlab.unittest.TestCase
+    %CONSTRACTORTEST Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    methods (Test)
+    end
+    
+end
+
